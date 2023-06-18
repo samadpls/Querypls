bc3b759930774c2055f88fcfc824ddd6f6e58fc8
Abdul Samad Siddiqui <abdulsamadsid1@gmail.com>
2023-06-18 20:08:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="Querypls",
 def main():
     with open('styles.css') as f:
         st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
-    st.markdown("""<a href='https://github.com/samadpls/yt-clickbait-detector'><img src='https://img.shields.io/github/stars/samadpls/querypls?color=red&label=star%20me&logoColor=red&style=social'></a>""",unsafe_allow_html=True) 
+    st.markdown("""<a href='https://github.com/samadpls/Querypls'><img src='https://img.shields.io/github/stars/samadpls/querypls?color=red&label=star%20me&logoColor=red&style=social'></a>""",unsafe_allow_html=True) 
     img , heading =  st.columns([1,8]) # using columns to display the heading and image
     with img:
         st.image("logo/logo.png",width=70) # logo
