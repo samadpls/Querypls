3191af8eb61ab4b9f627b1dbfc6f50728bc2cdf5
samadpls <abdulsamadsid1@gmail.com>
2023-11-10 00:18:43 +0500

    intial version-1.1

--- auth.py
+++ auth.py
@@ -6,17 +6,23 @@ CLIENT_ID = "386097294295-cshguegcn7krddrt1mok2srpsqej1d58.apps.googleuserconten
 CLIENT_SECRET = "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"
 REDIRECT_URI = "http://localhost:8501/"
 
+
 async def get_authorization_url(client: GoogleOAuth2, redirect_uri: str):
-    authorization_url = await client.get_authorization_url(redirect_uri, scope=["profile", "email"])
+    authorization_url = await client.get_authorization_url(
+        redirect_uri, scope=["profile", "email"]
+    )
     return authorization_url
+
 
 async def get_access_token(client: GoogleOAuth2, redirect_uri: str, code: str):
     token = await client.get_access_token(code, redirect_uri)
     return token
 
+
 async def get_email(client: GoogleOAuth2, token: str):
     user_id, user_email = await client.get_id_email(token)
     return user_id, user_email
+
 
 def get_login_str():
     client: GoogleOAuth2 = GoogleOAuth2(CLIENT_ID, CLIENT_SECRET)
