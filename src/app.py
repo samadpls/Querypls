d1f53402e123141c046bd9a1395028b0e41315b1
Myrausman <maira.usman5703@gmail.com>
2023-12-13 01:38:37 +0500

    Refactored UI 🎨

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ from langchain.prompts import PromptTemplate
 import streamlit as st
 from streamlit_oauth import OAuth2Component
 from deta import Deta
+from streamlit.runtime import caching
+from streamlit import runtime
 import sys
 import time
 import os
@@ -26,8 +28,6 @@ def configure_page_styles():
     Ensure 'static/css/styles.css' exists with desired styles.
     """
     st.set_page_config(page_title="Querypls", page_icon="💬")
-    with open("static/css/styles.css") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     hide_streamlit_style = (
         """<style>#MainMenu {visibility: hidden;}footer {visibility: hidden;}</style>"""
     )
@@ -229,19 +229,16 @@ def display_welcome_message():
     """Displays a welcome message based on user chat history."""
     no_chat_history = len(st.session_state.messages) == 1
     if no_chat_history:
-        if "user_email" in st.session_state:
-            st.markdown(
-                f"""#### Welcome `{st.session_state.user_email}` to \n ## 🛢💬Querypls - Prompt to SQL"""
-            )
-        else:
-            st.markdown(f"#### Welcome to \n ## 🛢💬Querypls - Prompt to SQL")
+        st.markdown(f"#### Welcome to \n ## 🛢💬Querypls - Prompt to SQL")
 
 
 def main():
     """Main function to configure and run the Querypls application."""
     configure_page_styles()
     deta = Deta(DETA_PROJECT_KEY)
-    llm = create_huggingface_hub()
+    if "model" not in st.session_state:
+        llm = create_huggingface_hub()
+        st.session_state["model"] = llm
     db = deta.Base("users")
     oauth2 = OAuth2Component(
         CLIENT_ID,
@@ -255,50 +252,67 @@ def main():
         st.session_state.code = False
 
     hide_main_menu_and_footer()
+    if st.session_state.code == False:
+        with st.container():
+            display_github_badge()
+            display_logo_and_heading()
 
-    with st.sidebar:
-        display_github_badge()
-        display_logo_and_heading()
-        st.markdown("`Made with 🤍 by samadpls`")
-        if "token" not in st.session_state:
-            result = oauth2.authorize_button(
-                "Connect with Google",
-                REDIRECT_URI,
-                SCOPE,
-                icon="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 48 48'%3E%3Cdefs%3E%3Cpath id='a' d='M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z'/%3E%3C/defs%3E%3CclipPath id='b'%3E%3Cuse xlink:href='%23a' overflow='visible'/%3E%3C/clipPath%3E%3Cpath clip-path='url(%23b)' fill='%23FBBC05' d='M0 37V11l17 13z'/%3E%3Cpath clip-path='url(%23b)' fill='%23EA4335' d='M0 11l17 13 7-6.1L48 14V0H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%2334A853' d='M0 37l30-23 7.9 1L48 0v48H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%234285F4' d='M48 48L17 24l-4-3 35-10z'/%3E%3C/svg%3E",
-                use_container_width=True,
-            )
-            handle_google_login_if_needed(result)
-            time.sleep(1)
-            st.rerun()
-        if st.session_state.code:
-            handle_new_chat(db)
-        if st.session_state.code:
-            display_previous_chats(db)
+            st.markdown("`Made with 🤍 by samadpls`")
+            if "token" not in st.session_state:
+                result = oauth2.authorize_button(
+                    "Connect with Google",
+                    REDIRECT_URI,
+                    SCOPE,
+                    icon="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 48 48'%3E%3Cdefs%3E%3Cpath id='a' d='M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z'/%3E%3C/defs%3E%3CclipPath id='b'%3E%3Cuse xlink:href='%23a' overflow='visible'/%3E%3C/clipPath%3E%3Cpath clip-path='url(%23b)' fill='%23FBBC05' d='M0 37V11l17 13z'/%3E%3Cpath clip-path='url(%23b)' fill='%23EA4335' d='M0 11l17 13 7-6.1L48 14V0H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%2334A853' d='M0 37l30-23 7.9 1L48 0v48H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%234285F4' d='M48 48L17 24l-4-3 35-10z'/%3E%3C/svg%3E",
+                    use_container_width=True,
+                )
+                handle_google_login_if_needed(result)
 
-    if "messages" not in st.session_state:
-        create_message()
-    display_welcome_message()
+    else:
+        with st.sidebar:
+            display_github_badge()
+            display_logo_and_heading()
+            st.markdown("`Made with 🤍 by samadpls`")
+            if "token" not in st.session_state:
+                result = oauth2.authorize_button(
+                    "Connect with Google",
+                    REDIRECT_URI,
+                    SCOPE,
+                    icon="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 48 48'%3E%3Cdefs%3E%3Cpath id='a' d='M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z'/%3E%3C/defs%3E%3CclipPath id='b'%3E%3Cuse xlink:href='%23a' overflow='visible'/%3E%3C/clipPath%3E%3Cpath clip-path='url(%23b)' fill='%23FBBC05' d='M0 37V11l17 13z'/%3E%3Cpath clip-path='url(%23b)' fill='%23EA4335' d='M0 11l17 13 7-6.1L48 14V0H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%2334A853' d='M0 37l30-23 7.9 1L48 0v48H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%234285F4' d='M48 48L17 24l-4-3 35-10z'/%3E%3C/svg%3E",
+                    use_container_width=True,
+                )
+                handle_google_login_if_needed(result)
+                time.sleep(1)
+                st.rerun()
+            if st.session_state.code:
+                handle_new_chat(db)
+            if st.session_state.code:
+                display_previous_chats(db)
 
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"], unsafe_allow_html=True)
+        if "messages" not in st.session_state:
+            create_message()
+        display_welcome_message()
 
-    if prompt := st.chat_input(disabled=(st.session_state.code is False)):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.write(prompt)
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"], unsafe_allow_html=True)
 
-        prompting = PromptTemplate(template=TEMPLATE, input_variables=["question"])
-        llm_chain = LLMChain(prompt=prompting, llm=llm)
+        if prompt := st.chat_input(disabled=(st.session_state.code is False)):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.write(prompt)
 
-        if st.session_state.messages[-1]["role"] != "assistant":
-            with st.chat_message("assistant"):
-                with st.spinner("Generating..."):
-                    response = llm_chain.run(prompt)
-                    st.markdown(response, unsafe_allow_html=True)
-                    message = {"role": "assistant", "content": response}
-                    st.session_state.messages.append(message)
+            prompting = PromptTemplate(template=TEMPLATE, input_variables=["question"])
+            if "model" in st.session_state:
+                llm_chain = LLMChain(prompt=prompting, llm=st.session_state.model)
+
+                if st.session_state.messages[-1]["role"] != "assistant":
+                    with st.chat_message("assistant"):
+                        with st.spinner("Generating..."):
+                            response = llm_chain.run(prompt)
+                            st.markdown(response, unsafe_allow_html=True)
+                            message = {"role": "assistant", "content": response}
+                            st.session_state.messages.append(message)
 
 
 if __name__ == "__main__":
